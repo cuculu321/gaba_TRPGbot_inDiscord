70510379e94c0c80b7898f6cff461e4606544a71
cuculu321 <matiyu321@gmail.com>
2020-02-25 18:22:45 +0900

    ✨ def split_space & bot_switch

--- trpg_bot.py
+++ trpg_bot.py
@@ -10,6 +10,23 @@ channel_id = 681676739310780436
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
+def parse_space(message_content):
+    return message_content.split()
+
+def bot_switch(message):
+    #botのモードをコマンドによってスイッチ
+    print(message.content)
+    if message.content.startswith('/neko'):
+        return('にゃーん')
+    
+    elif message.content.startswith('/dice'):
+    #サイコロを振るコマンド
+        message_splitd_space = print(parse_space(message.content))
+        return ("roll")
+
+    else:
+        return None
+
 # 起動時に動作する処理
 @client.event
 async def on_ready():
@@ -22,10 +39,11 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
+
     if message.channel.id == channel_id:
-        if message.content == '/neko':
-            await message.channel.send('にゃーん')
+        return_message = bot_switch(message)
+
+        await message.channel.send(return_message)
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
