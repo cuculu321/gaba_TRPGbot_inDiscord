040db3253f2f4b1dec679c5895207c272b6be65b
cuculu321 <matiyu321@gmail.com>
2020-02-26 15:15:28 +0900

    ✨ compleate player action cmd

--- trpg_bot.py
+++ trpg_bot.py
@@ -50,6 +50,20 @@ def parse_d(diceroll_cmd):
 def dice_roll(num_dice, dice_faces):
     return random.randint(num_dice, num_dice * dice_faces)
 
+def action_check(skill_point, dice):
+    if dice < skill_point:
+        if dice < 5:
+            return "クリティカル"
+        elif dice < 10:
+            return "スペシャル"
+        else:
+            return "成功"
+
+    else:
+        if dice > 95:
+            return "ファンブル"
+        else:
+            return "失敗"
 
 def bot_switch(message):
     #botのモードをコマンドによってスイッチ
@@ -62,15 +76,21 @@ def bot_switch(message):
         message_splitd_space = parse_space(message.content)
         num_dice, dice_faces = parse_d(message_splitd_space[1])
         rolled = dice_roll(int(num_dice), int(dice_faces))
-        return (message_splitd_space[1] + " => **" + str(rolled) +"**")
+        return (message_splitd_space[1] + " → **" + str(rolled) +"**")
 
     elif message.content.startswith('/act'):
-        cmd, player_name,  action = parse_space(message.content)
+    #プレイヤーが行動を行うときのコマンド
+        cmd, player_name, action = parse_space(message.content)
         worksheet = workbook.worksheet(player_name)
         act_cell = worksheet.find(action)
-        act_skill_point = worksheet.cell(act_cell.row, act_cell.col + 4).value
+        act_skill_point = int(worksheet.cell(act_cell.row, act_cell.col + 4).value)
 
-        return act_skill_point
+        dice = dice_roll(1, 100)
+        
+        act_result = action_check(act_skill_point, dice)
+
+        return (player_name + " の " + action + "(" + str(act_skill_point) + ") → **"
+                    + str(dice) +" "+ act_result + "**")
 
     else:
         return None
