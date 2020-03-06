6e6fd13c71f1114a130749bc5de41ad196ffbc49
cuculu321 <matiyu321@gmail.com>
2020-03-06 18:21:49 +0900

    ♻️ divide by file

--- trpg_bot.py
+++ trpg_bot.py
@@ -1,14 +1,12 @@
 # coding: utf-8
 import random
 import json
-import time
 import re
 
-#ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
-from oauth2client.service_account import ServiceAccountCredentials 
-import gspread
 import discord
 
+from read_spreadsheet import *
+from madness_list import *
 
 # *** Discordのボットの設定
 # 自分のBotのアクセストークンに置き換えてください
@@ -20,108 +18,6 @@ channel_id = [681676739310780436, 497063980385435681, 683269397095514166]
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
-
-#*** 一時的狂気リスト
-temporary_madness = {}
-temporary_madness[1] = '鸚鵡返し（誰かの動作・発言を真似することしか出来なくなる）'
-temporary_madness[2] = '健忘症（1d6時間以内のことを忘れる）'
-temporary_madness[3] = '多弁症（何があってもひたすら喋り続ける）'
-temporary_madness[4] = '偏食症（奇妙なものを食べたくなる）'
-temporary_madness[5] = '頭痛・嘔吐などの体調不良（技能値に-5）'
-temporary_madness[6] = '暴力癖（誰彼構わず暴力を振るう）'
-temporary_madness[7] = '幻聴或いは一時的難聴（聞き耳半減。この症状の探索者に精神分析や説得などを試みる場合は技能値に-10）'
-temporary_madness[8] ='逃亡癖（その場から逃げようとする）'
-temporary_madness[9] = '吃音や失声などの発語障害（交渉技能の技能値が半減する）'
-temporary_madness[10] = '不信（単独行動をとりたがる。交渉技能不可。）'
-temporary_madness[11] = '恐怖による行動不能'
-temporary_madness[12] = '自傷癖（自傷行動を行う。ラウンドごと1d2のダメージ判定を行う）'
-temporary_madness[13] = '感情の噴出（泣き続ける、笑い続けるなど。自発行動が出来なくなる）'
-temporary_madness[14] = '気絶（精神分析・またはCON*5のロールに成功で目覚める）'
-temporary_madness[15] = '幻覚あるいは妄想（目を使う技能は技能値に-30）'
-temporary_madness[16] = '偏執症（特定のものや行動に強く執着する）'
-temporary_madness[17] = 'フェティシズム（特定のものに性的魅惑を感じる）'
-temporary_madness[18] = '退行（乳幼児のような行動をとってしまう）'
-temporary_madness[19] = '自己愛（自分を守るために何でもしようとする）'
-temporary_madness[20] = '過信（自分を全能と信じて、どんなことでもしてしまう）'
-
-#*** 不定期の狂気リスト
-indefinite_madness = {}
-indefinite_madness[1] = '失語症（言葉を使う技能が使えなくなる）'
-indefinite_madness[2] = '心因性難聴（聞き耳不可。精神分析を受ける際に技能値に-30）'
-indefinite_madness[3] = '奇妙な性的嗜好（性的倒錯。特定のものに性的興奮を覚える）'
-indefinite_madness[4] = '偏執症（特定のものや行動に異常に執着する）'
-indefinite_madness[5] = '脱力・虚脱（自力での行動が出来なくなる）'
-indefinite_madness[6] = '恐怖症（特定のものに強い恐怖を覚える。そのものが側に存在する場合、技能値に-20）'
-indefinite_madness[7] = '自殺癖（ラウンドごとに1d4+1のダメージ判定を行う）'
-indefinite_madness[8] = '不信（単独行動をとりたがる。交渉技能不可。）'
-indefinite_madness[9] = '幻覚（目を使う技能は技能値に-30）'
-indefinite_madness[10] = '殺人癖（誰彼構わず殺そうとする） '
-
-class open_google_spreadsheet:
-    """
-    Google Spreadsheetのトークンやワークブック、ログインリセット関数などを保持する
-
-    Attributes
-    ----------
-    gc : gspread.Client
-        認証を受けログインしたクライアント情報
-    workbook : Spreadsheet
-        キーで指定したスプレッドシート
-    SPREADSHEET_KEY : String
-        使いたいスプレッドシートのキー
-    set_token_time : int
-        トークンを生成してからの時間
-    """
-
-    gc = []
-    workbook = []
-    SPREADSHEET_KEY = '1ThG04nz4l-ISa504UNcF97gKlkMx75YtggMGSJR2Eic'
-    set_token_time = 0
-    
-    def gs_login(self):
-        """
-        oauth2を用いてGoogleからClientを取得する
-        """
-
-        # *** Google SpreadSheetへのアクセス
-        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-
-        #認証情報設定
-        #ダウンロードしたjsonファイル名を設定
-        credentials = ServiceAccountCredentials.from_json_keyfile_name('gaba-cocbot-readspreadsheet-22b6a04f8d0a.json', scope)
-
-        #OAuth2の資格情報を使用してGoogle APIにログインします。
-        self.gc = gspread.authorize(credentials)
-
-    def open_workbook(self):
-        """
-        Client情報とSPREADSHEET_KEYを使ってスプレッドシートを開く
-        """
-
-        self.workbook = self.gc.open_by_key(self.SPREADSHEET_KEY)
-
-    def set_token(self):
-        """
-        スプレッドシートへのアクセスとアクセストークンを発行した時刻を記録
-        """
-
-        self.gs_login()
-        self.open_workbook()
-        self.set_token_time = time.time()
-
-    def give_workbook(self):
-        """
-        取得しているスプレッドシートを与える。
-        与える時に、トークンを発行してから一定時間立っている場合再発行
-        """
-
-        print(time.time() - self.set_token_time)
-        if (time.time() - self.set_token_time)> 3500:
-            print("refresh token")
-            self.set_token()
-
-        return self.workbook
-
 
 #*** スプレッドシートを使用するためのクラスと、トークンのセット
 acccess_spreadsheet = open_google_spreadsheet()
@@ -214,36 +110,6 @@ def action_check(skill_point, dice):
         else:
             return "失敗"
 
-def read_skill_point(workbook, player_name, action):
-    """
-    技能値の取得
-
-    Parameters
-    ----------
-    workbook : spreadsheet
-        取得したいspreadsheet
-    player_name : string
-        /actコマンドの第1引数。プレイヤーの名称(検索するシート名)
-    action : string
-        /actコマンドの第2引数。技能名(検索する文字)
-
-    Returns
-    -------
-    act_skill_point : int
-        技能値
-
-    """
-    
-    action_cmd = action
-    multipl_point = 1
-        
-    worksheet = workbook.worksheet(player_name)
-    act_cell = worksheet.find(action_cmd)
-
-    act_skill_point = int(worksheet.cell(act_cell.row, act_cell.col + 4).value)
-
-    return act_skill_point
-
 def search_operational_symbol(action):
     """
     文字列から四則演算記号の位置を取得する
@@ -265,6 +131,33 @@ def search_operational_symbol(action):
         return 0
     else:
         return search_result_obj.start()
+
+def splitting_action(action, index):
+    """
+    四則演算記号を元に分割を行う
+
+    Parameters
+    ----------
+    action : string
+        /actコマンドの第2引数。技能名(検索する文字)
+    index : int
+        四則演算記号のあるindex
+
+    Returns
+    -------
+    splited_action : string
+        技能名
+    operational_symbol : string
+        四則演算記号
+    arithmetic_num : int
+        修正値
+    """
+    
+    splited_action = action[0 : index]
+    operational_symbol = action[index]
+    arithmetic_num = action[index + 1 : len(action)]
+
+    return splited_action, operational_symbol, int(arithmetic_num)
 
 def four_arithmetic_operations(skill_point, symbol, num):
     """
@@ -294,33 +187,6 @@ def four_arithmetic_operations(skill_point, symbol, num):
         return skill_point - num
     elif symbol == "/":
         return skill_point / num
-
-def splitting_action(action, index):
-    """
-    四則演算記号を元に分割を行う
-
-    Parameters
-    ----------
-    action : string
-        /actコマンドの第2引数。技能名(検索する文字)
-    index : int
-        四則演算記号のあるindex
-
-    Returns
-    -------
-    splited_action : string
-        技能名
-    operational_symbol : string
-        四則演算記号
-    arithmetic_num : int
-        修正値
-    """
-    
-    splited_action = action[0 : index]
-    operational_symbol = action[index]
-    arithmetic_num = action[index + 1 : len(action)]
-
-    return splited_action, operational_symbol, int(arithmetic_num)
 
 def bot_switch(message):
     """
@@ -376,11 +242,8 @@ def bot_switch(message):
                         + str(dice) +" "+ act_result + "**")
 
         action, operational_symbol, arithmetic_num = splitting_action(action, operational_index)
-
         act_skill_point = read_skill_point(workbook, player_name, action)
-
         act_skill_point = four_arithmetic_operations(act_skill_point, operational_symbol, arithmetic_num)
-
         dice = dice_roll(1, 100)
         act_result = action_check(act_skill_point, dice)
 
