a28600befde2ef03087e7b69c3bd8c80f2505727
cuculu321 <matiyu321@gmail.com>
2020-03-06 14:04:53 +0900

    📝 about spreadsheet_class

--- trpg_bot.py
+++ trpg_bot.py
@@ -58,32 +58,63 @@ indefinite_madness[9] = '幻覚（目を使う技能は技能値に-30）'
 indefinite_madness[10] = '殺人癖（誰彼構わず殺そうとする） '
 
 class open_google_spreadsheet:
+    """
+    Google Spreadsheetのトークンやワークブック、ログインリセット関数などを保持する
+
+    Attributes
+    ----------
+    gc : gspread.Client
+        認証を受けログインしたクライアント情報
+    workbook : Spreadsheet
+        キーで指定したスプレッドシート
+    SPREADSHEET_KEY : String
+        使いたいスプレッドシートのキー
+    set_token_time : int
+        トークンを生成してからの時間
+    """
+
     gc = []
     workbook = []
     SPREADSHEET_KEY = '1ThG04nz4l-ISa504UNcF97gKlkMx75YtggMGSJR2Eic'
     set_token_time = 0
     
     def gs_login(self):
+        """
+        oauth2を用いてGoogleからClientを取得する
+        """
+
         # *** Google SpreadSheetへのアクセス
-        #2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
         scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
         #認証情報設定
-        #ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
+        #ダウンロードしたjsonファイル名を設定
         credentials = ServiceAccountCredentials.from_json_keyfile_name('gaba-cocbot-readspreadsheet-22b6a04f8d0a.json', scope)
 
         #OAuth2の資格情報を使用してGoogle APIにログインします。
         self.gc = gspread.authorize(credentials)
 
     def open_workbook(self):
+        """
+        Client情報とSPREADSHEET_KEYを使ってスプレッドシートを開く
+        """
+
         self.workbook = self.gc.open_by_key(self.SPREADSHEET_KEY)
 
     def set_token(self):
+        """
+        スプレッドシートへのアクセスとアクセストークンを発行した時刻を記録
+        """
+
         self.gs_login()
         self.open_workbook()
         self.set_token_time = time.time()
 
     def give_workbook(self):
+        """
+        取得しているスプレッドシートを与える。
+        与える時に、トークンを発行してから一定時間立っている場合再発行
+        """
+
         print(time.time() - self.set_token_time)
         if (time.time() - self.set_token_time)> 3500:
             print("refresh token")
